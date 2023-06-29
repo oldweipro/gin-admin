7dbbd4655fb2347120763e03f347714bdd99a183
oldweipro <oldweipro@gmail.com>
2023-06-29 21:50:11 +0800

    fix nil

--- router/platform/open.go
+++ router/platform/open.go
@@ -12,9 +12,12 @@ type OpenApiRouter struct {
 // InitPlatformRouter InitConversationRouter 初始化 Conversation 路由信息
 func (p *OpenApiRouter) InitPlatformRouter(Router *gin.RouterGroup) {
 	platformRouter := Router.Group("v1").Use(middleware.OperationRecord())
+	platformRouterWithoutOperationRecord := Router.Group("v1")
 	var platformApi = v1.ApiGroupApp.PlatformApiGroup.OpenApi
 	{
-		platformRouter.POST("chat/completions", platformApi.ForwardChatCompletionsApi)           // 获取当前用户聊天列表
-		platformRouter.OPTIONS("chat/completions", platformApi.ForwardOptionsChatCompletionsApi) // 获取当前用户聊天列表
+		platformRouter.POST("chat/completions", platformApi.ForwardChatCompletionsApi) // 获取当前用户聊天列表
+	}
+	{
+		platformRouterWithoutOperationRecord.OPTIONS("chat/completions", platformApi.ForwardOptionsChatCompletionsApi) // 获取当前用户聊天列表
 	}
 }
