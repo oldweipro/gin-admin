b4962c858b136e1197d38cfa1878db36a446d75b
oldweipro <oldweipro@gmail.com>
2023-09-05 13:24:20 +0800

    update model form

--- global/model.go
+++ global/model.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Model struct {
-	ID        uint           `json:"id" gorm:"primarykey"` // 主键ID
-	CreatedAt time.Time      `json:"createdAt"`            // 创建时间
-	UpdatedAt time.Time      `json:"updatedAt"`            // 更新时间
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`       // 删除时间
+	ID        uint           `json:"id" form:"id" gorm:"primarykey"` // 主键ID
+	CreatedAt time.Time      `json:"createdAt" form:"createdAt"`     // 创建时间
+	UpdatedAt time.Time      `json:"updatedAt" form:"updatedAt"`     // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`                 // 删除时间
 }
